a656f7f774a8308f156c5c6e5163215b1b868ba6
julie-cruz <jrresendiz@cps.edu>
2025-01-10 11:48:21 -0600

    Add files via upload
    This file codes for a game/quiz that allows the user to determine their Godly parent as if in the universe of Greek Mythology. They can choose between having the parent be a God or a Goddess. They will get a result based on their answer choices which relate to the users personal preferences.

---
+++ namegenerator (1).py
@@ -0,0 +1,35 @@
+#juliana resendiz
+
+#name generator project
+
+print("Welcome to finding out who your Greek Mythology Godly parent would be?")
+print("Begin answering the following questions to start!!")
+ans = input("who is your godly parent your mother(m) or father(f)")
+if ans == "m":
+    ans = input("What color appeals to you the most, bubblegum pink(bp) or midnight blue(mb)")
+    if ans == "bp":
+        ans = input("What aspect of nature do you prefer, meadows(m) or flowers(f)?")
+        if ans == "m":
+            ans = input("Congratulations your greek parent is: Hera, the mother of gods and the goddess of women, marriage, family and childbirth!")
+        if ans == "f":
+            ans = input("Congratulations your greek parent is: Aphrodite, the goddess of love, beauty, and desire!")
+    if ans == "mb":
+        ans = input("What aspect of nature do you prefer, mountains(m) or forests(f)?")
+        if ans == "m":
+            ans = input("Congratulations your greek parent is: Athena, the goddess of war, handicraft, and practical reason!")
+        if ans == "f":
+            ans = input("Congratulations your greek parent is: Artemis, the godess of the hunt, the moon, and chastity!")
+if ans =="f":
+    ans = input("What color appeals to you the most, gold(g) or crimson red(cr)")
+    if ans == "g":
+        ans = input("What aspect of nature do you prefer, oceans(o) or sunlight(s)?")
+        if ans == "o":
+            ans = input("Congratulations your greek parent is: Poseidon, the god of the seas!")
+        if ans == "s":
+            ans = input("Congratulations your greek parent is: Apollo, the god of music, light, and prophecy!")
+    if ans == "cr":
+        ans = input("What aspect of nature do you prefer, volcanoes(v) or thunder(t)?")
+        if ans == "v":
+            ans = input("Congratulations your greek parent is: Hephaestus, the god of blacksmiths and craftsmanship!")
+        if ans == "t":
+            ans = input("Congratulations your greek parent is: Ares, the god of war!")
