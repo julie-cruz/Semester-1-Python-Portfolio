3ef54887b07dfac7f69c311de6dbb85e3ef022d2
julie-cruz <jrresendiz@cps.edu>
2025-01-10 11:52:13 -0600

    Add files via upload
    This file contains the code for a Bedtime story formulator. With inputs from the user, they can choose what they want to be a part of the story. This code allows the user to have creative freedom and then will generate a story based on those inputs.

---
+++ bedtimestory.py
@@ -0,0 +1,21 @@
+#juliana resendiz
+
+
+
+
+#functions
+
+def bedtime_story():
+    print("Hello, Welcome to MadLibs Bedtime Story generator!")
+    print("Please input the following information asked of you to help generate your story!")
+    name = input("Please enter a girls name: ")
+    adjective = input("Please enter an adjective")
+    action = input("Please enter an action: ")
+    bpart = input("Please enter a body part: ")
+    location = input("Please enter the name where kids usualy go to")
+    print("On one bright and sunny day "+adjective+" " +name+" was out and about having a good time, as she headed towards " +location)
+    print("Unfortunately she "+action+" and fell on her "+bpart+" Fortunately was alright and proceeded about her day as happy as she started it!")
+
+
+#main
+bedtime_story()
